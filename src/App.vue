711b5ab1313f9c202c5b32ab177a12027a0f7ded
luxlinna <luxlinna@gmail.com>
2021-03-24 00:37:31 +0100

    commit files

--- src/App.vue
+++ src/App.vue
@@ -12,18 +12,26 @@
     </div>
     
     <router-view/>
-    <div id="footer" class="p-4 text-center">
+    <div id="footer" class="p-2 text-center">      
         <footer class="text-light p-4">
             <a href="https://lms.se.beetroot.academy/myCourses/lesson/ckhxhrznu61d60880bj0nj9ln" target="_blank" class="text-warning">Build This Pages</a> |
             <a href="https://app.slack.com/client/T019E5XNM4Y/D01GB3DGE9G" class="text-warning">Privacy Policy</a>
             <br>
             <small>&copy; 2021, Linna</small>
+           <div class="my-icons mt-3">
+              <a href="https://www.facebook.com" class="fa fa-facebook ml-4 p-2"></a>
+              <a href="https://twitter.com" class="fa fa-twitter ml-4 p-2"></a>
+              <a href="https://www.google.com" class="fa fa-google ml-4 p-2"></a>
+              <a href="https://www.linkedin.com" class="fa fa-linkedin ml-4 p-2"></a>
+              <a href="https://www.youtube.com" class="fa fa-youtube ml-4 p-2"></a>
+           </div>
         </footer>
     </div>
   </div>
 </template>
 
 <style>
+@import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css');
 #app {
   font-family: Roboto, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -60,4 +68,48 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+
+/* Start my icons and colors style */
+.my-icons {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  flex-wrap: nowrap; /* assumes you only want one row */
+}
+
+.fa {  
+	width: 30px;
+	padding: 7px;
+	font-size: 20px;
+	text-align: center;
+	text-decoration: none;
+	margin: 2px 2px;
+	color: white;
+	border-radius: 50%; 
+}
+
+.fa:hover {
+	opacity: 0.7;
+}
+
+.fa-facebook {
+	background: #3B5998;
+}
+
+.fa-twitter {
+	background: #55ACEE;
+}
+
+.fa-google {
+	background: #dd4b39;
+}
+
+.fa-linkedin {
+	background: #007bb5;
+}
+
+.fa-youtube {
+	background: #bb0000;
+}
+/* End my icons and colors style */
 </style>
